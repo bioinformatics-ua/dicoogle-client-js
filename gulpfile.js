00c45b8f69cf7a476f3b7cc2d3af4a39a2cc327d
Eduardo Pinho <eduardopinho@ua.pt>
2019-07-15 15:52:52 +0100

    Update test coverage
    - use latest nyc and coveralls directly
    - clean up and format gulpfile
    - WIP, some files are not being covered

--- gulpfile.js
+++ gulpfile.js
@@ -17,143 +17,90 @@
  * along with Dicoogle.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-'use strict';
+"use strict";
 
-const browserify = require('browserify');
-const buffer = require('vinyl-buffer');
-const coveralls = require('gulp-coveralls');
-const eslint = require('gulp-eslint');
-const fs = require('fs');
-const gulp = require('gulp');
-const header = require('gulp-header');
-const istanbul = require('gulp-istanbul');
-const mocha = require('gulp-mocha');
-const remapIstanbul = require('remap-istanbul/lib/gulpRemapIstanbul');
-const rm = require('gulp-rm');
-const source = require('vinyl-source-stream');
-const sourcemaps = require('gulp-sourcemaps');
-const ts = require('gulp-typescript');
+const browserify = require("browserify");
+const buffer = require("vinyl-buffer");
+const eslint = require("gulp-eslint");
+const fs = require("fs");
+const gulp = require("gulp");
+const header = require("gulp-header");
+const mocha = require("gulp-mocha");
+const rm = require("gulp-rm");
+const source = require("vinyl-source-stream");
+const sourcemaps = require("gulp-sourcemaps");
+const ts = require("gulp-typescript");
 const tsProject = ts.createProject("tsconfig.json");
-const uglify = require('gulp-uglify');
+const uglify = require("gulp-uglify");
 
 var _licenseText = null;
 function licenseText() {
   if (!_licenseText) {
-    _licenseText = fs.readFileSync('license-header.txt', 'UTF-8');
+    _licenseText = fs.readFileSync("license-header.txt", "UTF-8");
   }
   return _licenseText;
 }
 
 function lint() {
-  return gulp.src(['src/*.js', 'test/*.js', 'test/mock/*.js', 'bin/*.js'])
+  return gulp
+    .src(["src/*.js", "test/*.js", "test/mock/*.js", "bin/*.js"])
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
 }
 
-gulp.task('lint', lint);
+gulp.task("lint", lint);
 
 function main() {
-  return tsProject.src()
-       .pipe(buffer())
-       .pipe(sourcemaps.init({loadMaps: true}))
-       .pipe(tsProject())
-       .pipe(sourcemaps.write('./'))        
-       .pipe(gulp.dest("lib"));
+  return tsProject
+    .src()
+    .pipe(buffer())
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(tsProject())
+    .pipe(sourcemaps.write("./"))
+    .pipe(gulp.dest("lib"));
 }
-gulp.task('main', gulp.series(lint, main));
+gulp.task("main", gulp.series(lint, main));
 
 function bundle() {
   // set up the browserify instance on a task basis
   var b = browserify({
-    entries: './lib/index.js',
+    entries: "./lib/index.js",
     debug: false,
     transform: [
-        ['envify', {
-            _: 'purge',
-            NODE_ENV: 'production'
-        }]
+      [
+        "envify",
+        {
+          _: "purge",
+          NODE_ENV: "production"
+        }
+      ]
     ]
   });
-  return b.bundle()
-    .pipe(source('dicoogle-client.min.js'))
+  return b
+    .bundle()
+    .pipe(source("dicoogle-client.min.js"))
     .pipe(buffer())
     .pipe(uglify())
     .pipe(header(licenseText()))
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest("dist"));
 }
-gulp.task('bundle', gulp.series(lint, main, bundle));
+gulp.task("bundle", gulp.series(lint, main, bundle));
 
 function test() {
-  return gulp
-      .src([
-          'test/**/test-*.js'
-      ])
-      .pipe(mocha({
-          bail: true,
-          timeout: 50000
-      }));
+  return gulp.src(["test/**/test-*.js"]).pipe(
+    mocha({
+      bail: true,
+      timeout: 50000
+    })
+  );
 }
-gulp.task('test', test);
-
-function preCover() {
-  return gulp.src(['lib/**/*.js'])
-      .pipe(istanbul({
-          includeUntested: true
-      }))
-      .pipe(istanbul.hookRequire());
-}
-gulp.task('pre-cover', preCover);
-
-function cover() {
-  return gulp.src(['test/**/test-*.js'])
-      .pipe(mocha({
-          bail: true,
-          timeout: 50000
-      }))
-      // Creating the reports after tests ran
-      .pipe(istanbul.writeReports({
-          reporters: [ 'json' ]
-      }))
-      .pipe(istanbul.enforceThresholds({
-        thresholds: {
-          global: {
-            statements: 80,
-            lines: 70,
-            functions: -10
-          }
-        }
-      })).on('end', remapCoverageFiles);
-}
-gulp.task('cover', gulp.series(preCover, cover));
-
-function taskCoveralls() {
-  return gulp.src('coverage/lcov.info')
-      .pipe(coveralls());
-}
-gulp.task('coveralls', taskCoveralls);
-
-function remapCoverageFiles() {
-    return gulp.src('./coverage/coverage-final.json')
-      .pipe(remapIstanbul({
-          basePath: 'src',
-          reports: {
-              //'html': './coverage/lcov-report',
-              'text': null,
-              'text-summary': null,
-              'lcovonly': './coverage/lcov.info'
-          },
-          fail: true
-      }))
-      .pipe(gulp.dest('./coverage'));
-}
+gulp.task("test", test);
 
 function clean() {
-  return gulp.src(['dist/*', 'lib/*'], { read: false })
-    .pipe( rm() );
+  return gulp.src(["dist/*", "lib/*"], { read: false }).pipe(rm());
 }
-gulp.task('clean', clean);
+gulp.task("clean", clean);
 
-
-exports.default = gulp.series('main', 'bundle');
-gulp.task('default', exports.default);
+exports.default = gulp.series("main", "bundle");
+gulp.task("default", exports.default);
